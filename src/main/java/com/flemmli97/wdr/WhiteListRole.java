754141d0507b62eb34b1fcb1de72fd7b529765d2
Flemmli97 <Flemmli97@users.noreply.github.com>
2019-10-14 09:44:23 +0200

    initial commit

---
+++ src/main/java/com/flemmli97/wdr/WhiteListRole.java
@@ -0,0 +1,32 @@
+package com.flemmli97.wdr;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = WhiteListRole.MODID, name = WhiteListRole.MODNAME, version = WhiteListRole.VERSION, acceptableRemoteVersions = "*")
+@Mod.EventBusSubscriber
+public class WhiteListRole {
+
+    public static final String MODID = "wrl";
+    public static final String MODNAME = "Whitelist Role";
+    public static final String VERSION = "1.0.0";
+    public static final Logger logger = LogManager.getLogger(WhiteListRole.MODID);
+
+    @Instance
+    public static WhiteListRole instance = new WhiteListRole();   
+    
+    @EventHandler
+    public void serverStart(FMLServerStartingEvent event) {
+    	if(event.getServer().isDedicatedServer())
+    	{
+	    	event.registerServerCommand(new CommandAddWhitelistRole());
+	    	event.registerServerCommand(new CommandAddWhitelist());
+    	}
+    }
+}
+    
