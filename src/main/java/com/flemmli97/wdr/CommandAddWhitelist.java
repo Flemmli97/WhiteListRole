82aeffbe052e77ad3d8fc35757a0049bb1983253
Flemmli97 <Flemmli97@users.noreply.github.com>
2019-10-25 17:28:54 +0200

    format testing

--- src/main/java/com/flemmli97/wdr/CommandAddWhitelist.java
+++ src/main/java/com/flemmli97/wdr/CommandAddWhitelist.java
@@ -1,6 +1,5 @@
 package com.flemmli97.wdr;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -20,15 +19,14 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 
-public class CommandAddWhitelist implements ICommand{
+public class CommandAddWhitelist implements ICommand {
 
 	private final List<String> aliases = new ArrayList<String>();
 
-	public CommandAddWhitelist()
-	{
+	public CommandAddWhitelist() {
 		this.aliases.add("wlr:whitelist");
 	}
-	
+
 	@Override
 	public String getName() {
 		return "wlr:whitelist";
@@ -46,87 +44,75 @@ public class CommandAddWhitelist implements ICommand{
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if (args.length < 1)
-        {
-            throw new WrongUsageException(this.getUsage(sender), new Object[0]);
-        }
-        else
-        {
-            if ("list".equals(args[0]))
-            {
-                sender.sendMessage(new TextComponentTranslation("commands.whitelist.list", new Object[] {server.getPlayerList().getWhitelistedPlayerNames().length, server.getPlayerList().getAvailablePlayerDat().length}));
-                String[] astring = server.getPlayerList().getWhitelistedPlayerNames();
-                sender.sendMessage(new TextComponentString(CommandBase.joinNiceString(astring)));
-            }
-            else if ("add".equals(args[0]))
-            {
-                if (args.length < 2)
-                {
-                    throw new WrongUsageException("commands.whitelist.add.usage", new Object[0]);
-                }
+		if (args.length < 1) {
+			throw new WrongUsageException(this.getUsage(sender), new Object[0]);
+		} else {
+			if ("list".equals(args[0])) {
+				sender.sendMessage(new TextComponentTranslation("commands.whitelist.list",
+						new Object[] { server.getPlayerList().getWhitelistedPlayerNames().length,
+								server.getPlayerList().getAvailablePlayerDat().length }));
+				String[] astring = server.getPlayerList().getWhitelistedPlayerNames();
+				sender.sendMessage(new TextComponentString(CommandBase.joinNiceString(astring)));
+			} else if ("add".equals(args[0])) {
+				if (args.length < 2) {
+					throw new WrongUsageException("commands.whitelist.add.usage", new Object[0]);
+				}
 
-                GameProfile gameprofile = server.getPlayerProfileCache().getGameProfileForUsername(args[1]);
+				GameProfile gameprofile = server.getPlayerProfileCache().getGameProfileForUsername(args[1]);
 
-                if (gameprofile == null)
-                {
-                    throw new CommandException("commands.whitelist.add.failed", new Object[] {args[1]});
-                }
+				if (gameprofile == null) {
+					throw new CommandException("commands.whitelist.add.failed", new Object[] { args[1] });
+				}
 
-                server.getPlayerList().addWhitelistedPlayer(gameprofile);
-                CommandBase.notifyCommandListener(sender, this, "commands.whitelist.add.success", new Object[] {args[1]});
-            }
-            else if ("remove".equals(args[0]))
-            {
-                if (args.length < 2)
-                {
-                    throw new WrongUsageException("commands.whitelist.remove.usage", new Object[0]);
-                }
+				server.getPlayerList().addWhitelistedPlayer(gameprofile);
+				CommandBase.notifyCommandListener(sender, this, "commands.whitelist.add.success",
+						new Object[] { args[1] });
+			} else if ("remove".equals(args[0])) {
+				if (args.length < 2) {
+					throw new WrongUsageException("commands.whitelist.remove.usage", new Object[0]);
+				}
 
-                GameProfile gameprofile1 = server.getPlayerList().getWhitelistedPlayers().getByName(args[1]);
+				GameProfile gameprofile1 = server.getPlayerList().getWhitelistedPlayers().getByName(args[1]);
 
-                if (gameprofile1 == null)
-                {
-                    throw new CommandException("commands.whitelist.remove.failed", new Object[] {args[1]});
-                }
+				if (gameprofile1 == null) {
+					throw new CommandException("commands.whitelist.remove.failed", new Object[] { args[1] });
+				}
 
-                server.getPlayerList().removePlayerFromWhitelist(gameprofile1);
-                CommandBase.notifyCommandListener(sender, this, "commands.whitelist.remove.success", new Object[] {args[1]});
-            }
-        }
+				server.getPlayerList().removePlayerFromWhitelist(gameprofile1);
+				CommandBase.notifyCommandListener(sender, this, "commands.whitelist.remove.success",
+						new Object[] { args[1] });
+			}
+		}
 	}
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		if(sender.getCommandSenderEntity() instanceof EntityPlayer)
+		if (sender.getCommandSenderEntity() instanceof EntityPlayer)
 			return WhiteListData.get(server.getEntityWorld()).hasRole((EntityPlayer) sender.getCommandSenderEntity());
 		return false;
 	}
 
 	@Override
-	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
-    {
-        if (args.length == 1)
-        {
-            return CommandBase.getListOfStringsMatchingLastWord(args, new String[] {"add", "remove", "list"});
-        }
-        else
-        {
-            if (args.length == 2)
-            {
-                if ("remove".equals(args[0]))
-                {
-                    return CommandBase.getListOfStringsMatchingLastWord(args, server.getPlayerList().getWhitelistedPlayerNames());
-                }
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
+			@Nullable BlockPos targetPos) {
+		if (args.length == 1) {
+			return CommandBase.getListOfStringsMatchingLastWord(args, new String[] { "add", "remove", "list" });
+		} else {
+			if (args.length == 2) {
+				if ("remove".equals(args[0])) {
+					return CommandBase.getListOfStringsMatchingLastWord(args,
+							server.getPlayerList().getWhitelistedPlayerNames());
+				}
 
-                if ("add".equals(args[0]))
-                {
-                    return CommandBase.getListOfStringsMatchingLastWord(args, server.getPlayerProfileCache().getUsernames());
-                }
-            }
+				if ("add".equals(args[0])) {
+					return CommandBase.getListOfStringsMatchingLastWord(args,
+							server.getPlayerProfileCache().getUsernames());
+				}
+			}
 
-            return Collections.<String>emptyList();
-        }
-    }
+			return Collections.<String>emptyList();
+		}
+	}
 
 	@Override
 	public boolean isUsernameIndex(String[] args, int index) {
--- src/main/java/com/flemmli97/wdr/CommandAddWhitelistRole.java
+++ src/main/java/com/flemmli97/wdr/CommandAddWhitelistRole.java
@@ -1,6 +1,5 @@
 package com.flemmli97.wdr;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,15 +14,14 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 
-public class CommandAddWhitelistRole implements ICommand{
+public class CommandAddWhitelistRole implements ICommand {
 
 	private final List<String> aliases = new ArrayList<String>();
 
-	public CommandAddWhitelistRole()
-	{
+	public CommandAddWhitelistRole() {
 		this.aliases.add("wlr:addWhitelistRole");
 	}
-	
+
 	@Override
 	public String getName() {
 		return "wlr:addWhitelistRole";
@@ -41,63 +39,50 @@ public class CommandAddWhitelistRole implements ICommand{
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if(!args[0].equals("list") && args.length<2)
-        {
-            throw new WrongUsageException(this.getUsage(sender), new Object[0]);
-        }
-		else	
-		{
-			if(args[0].equals("list"))
-			{
-                Set<String> astring = WhiteListData.get(server.getEntityWorld()).players(server);
-				sender.sendMessage(new TextComponentString("People with whitelist permission: (" + astring.size() + "):"));
-                sender.sendMessage(new TextComponentString(joinNiceString(astring)));
-			}
-			else if(args[0].equals("remove"))
-			{
-				if(WhiteListData.get(server.getEntityWorld()).removePlayer(server, args[1]))
-	                CommandBase.notifyCommandListener(sender, this, "Removed whitelist permission for %s", new Object[] {args[1]});
-				else
-				{
-                    throw new CommandException("Couldnt remove permission for %s", new Object[] {args[1]});
+		if (!args[0].equals("list") && args.length < 2) {
+			throw new WrongUsageException(this.getUsage(sender), new Object[0]);
+		} else {
+			if (args[0].equals("list")) {
+				Set<String> astring = WhiteListData.get(server.getEntityWorld()).players(server);
+				sender.sendMessage(
+						new TextComponentString("People with whitelist permission: (" + astring.size() + "):"));
+				sender.sendMessage(new TextComponentString(joinNiceString(astring)));
+			} else if (args[0].equals("remove")) {
+				if (WhiteListData.get(server.getEntityWorld()).removePlayer(server, args[1]))
+					CommandBase.notifyCommandListener(sender, this, "Removed whitelist permission for %s",
+							new Object[] { args[1] });
+				else {
+					throw new CommandException("Couldnt remove permission for %s", new Object[] { args[1] });
 				}
-			}
-			else if(args[0].equals("add"))
-			{
-				if(WhiteListData.get(server.getEntityWorld()).addPlayer(server, args[1]))
-	                CommandBase.notifyCommandListener(sender, this, "Added whitelist permission for %s", new Object[] {args[1]});
-				else
-				{
-                    throw new CommandException("Couldnt add permission for $s", new Object[] {args[1]});
+			} else if (args[0].equals("add")) {
+				if (WhiteListData.get(server.getEntityWorld()).addPlayer(server, args[1]))
+					CommandBase.notifyCommandListener(sender, this, "Added whitelist permission for %s",
+							new Object[] { args[1] });
+				else {
+					throw new CommandException("Couldnt add permission for $s", new Object[] { args[1] });
 				}
 			}
 		}
 	}
-	
-	private static String joinNiceString(Set<String> elements)
-    {
-        StringBuilder stringbuilder = new StringBuilder();
 
-        int i = 0;
-        for(String s : elements)
-        {
-        	if (i > 0)
-            {
-                if (i == elements.size() - 1)
-                {
-                    stringbuilder.append(" and ");
-                }
-                else
-                {
-                    stringbuilder.append(", ");
-                }
-            }
+	private static String joinNiceString(Set<String> elements) {
+		StringBuilder stringbuilder = new StringBuilder();
 
-            stringbuilder.append(s);
-            i++;
-        }
-        return stringbuilder.toString();
-    }
+		int i = 0;
+		for (String s : elements) {
+			if (i > 0) {
+				if (i == elements.size() - 1) {
+					stringbuilder.append(" and ");
+				} else {
+					stringbuilder.append(", ");
+				}
+			}
+
+			stringbuilder.append(s);
+			i++;
+		}
+		return stringbuilder.toString();
+	}
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
@@ -107,27 +92,23 @@ public class CommandAddWhitelistRole implements ICommand{
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
 			BlockPos targetPos) {
-		if (args.length == 1)
-        {
-            return CommandBase.getListOfStringsMatchingLastWord(args, new String[] {"add", "remove", "list"});
-        }
-        else
-        {
-            if (args.length == 2)
-            {
-                if ("remove".equals(args[0]))
-                {
-                    return CommandBase.getListOfStringsMatchingLastWord(args, server.getPlayerList().getWhitelistedPlayerNames());
-                }
+		if (args.length == 1) {
+			return CommandBase.getListOfStringsMatchingLastWord(args, new String[] { "add", "remove", "list" });
+		} else {
+			if (args.length == 2) {
+				if ("remove".equals(args[0])) {
+					return CommandBase.getListOfStringsMatchingLastWord(args,
+							server.getPlayerList().getWhitelistedPlayerNames());
+				}
 
-                if ("add".equals(args[0]))
-                {
-                    return CommandBase.getListOfStringsMatchingLastWord(args, server.getPlayerProfileCache().getUsernames());
-                }
-            }
+				if ("add".equals(args[0])) {
+					return CommandBase.getListOfStringsMatchingLastWord(args,
+							server.getPlayerProfileCache().getUsernames());
+				}
+			}
 
-            return Collections.<String>emptyList();
-        }
+			return Collections.<String>emptyList();
+		}
 	}
 
 	@Override
